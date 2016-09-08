4bf0b1496d9e02edda8759a1c47904a2561a63bb
Jesper Wendel Devantier <git@pseudonymous.me>
2016-09-08 22:56:48 +0200

    first pass on mw enhancer

--- src/cljs/readux/middleware/log_model_diff.cljs
+++ src/cljs/readux/middleware/log_model_diff.cljs
@@ -4,16 +4,15 @@
   (:require-macros [readux.utils :as rdu]))
 
 (defn log-model-diff
-  [next]
-  (fn [model action & args]
-    (rdu/with-console-group
-      (str "Action['" (name action) "']")
-      (let [new-model (apply next model action args)
-            [removed added _] (diff model new-model)]
-        (rdu/with-console-group
-          "Added"
-          (pprint added))
-        (rdu/with-console-group
-          "Removed"
-          (pprint removed))
-        new-model))))
+  [dispatch next model action args]
+  (rdu/with-console-group
+    (str "Action['" (name action) "']")
+    (let [new-model (apply next model action args)
+          [removed added _] (diff model new-model)]
+      (rdu/with-console-group
+        "Added"
+        (pprint added))
+      (rdu/with-console-group
+        "Removed"
+        (pprint removed))
+      new-model)))

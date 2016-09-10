4a9dcba10aad97f0729b11dddcacafe8cebf097c
pseudo <git@pseudonymous.me>
2016-09-10 23:45:53 +0200

    log-model-diff: ensure logging is always on
    The function relied on cljs.pprint/pprint & println before which
    meant most output (non-group headers) were omitted unless
    (enable-console-print!) was called.
    
    This change (re)defines a set of logging functions calling the
    js console log/warn/error functions directly to avoid that
    requirement and ensure things are logged.

--- src/cljs/readux/middleware/log_model_diff.cljs
+++ src/cljs/readux/middleware/log_model_diff.cljs
@@ -3,20 +3,36 @@
             [cljs.pprint :refer [pprint]])
   (:require-macros [readux.utils :as rdu]))
 
+(defn- ppstr
+  [obj]
+  (with-out-str (cljs.pprint/pprint obj)))
+
+(defn- log
+  [& args]
+  (.apply (.-log js/console) js/console (into-array args)))
+
+(defn- warn
+  [& args]
+  (.apply (.-warn js/console) js/console (into-array args)))
+
+(defn- error
+  [& args]
+  (.apply (.-error js/console) js/console (into-array args)))
+
 (defn log-model-diff
   [dispatch next model action data]
   (rdu/with-console-group
     (str "Action['" (name action) "']")
     (when data
-      (print "---[DATA]---")
-      (cljs.pprint/pprint data)
-      (println (str "---[/DATA]---")))
+      (rdu/with-console-group
+        "Data"
+        (log (ppstr data))))
     (let [new-model (next model action data)
           [removed added _] (diff model new-model)]
       (rdu/with-console-group
         "Added"
-        (pprint added))
+        (log (ppstr added)))
       (rdu/with-console-group
         "Removed"
-        (pprint removed))
+        (log (ppstr removed)))
       new-model)))
